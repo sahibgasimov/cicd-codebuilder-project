f33e241a654ae826ade1ffaebc8d862a9844edf2
sahibgasimov <sahib.gasimov@yahoo.com>
2022-09-26 00:56:29 -0500

    added build

--- ec2.tf
+++ ec2.tf
@@ -6,15 +6,15 @@ provider "aws" {
 
 
 # store the terraform state file in s3
-/* terraform {
+ terraform {
   backend "s3" {
     bucket  = "aosnote-terraform-state-bucket-1"
     key     = "build/terraform.tfstate"
     region  = "us-east-1"
     /* profile = "terraform-user" */
   
-  #}
-#} */
+  }
+} 
 
 
 # create default vpc if one does not exit
